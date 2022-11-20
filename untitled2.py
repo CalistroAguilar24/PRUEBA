852a9151599739214ae807ef19d62665752bcb75
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-19 22:24:08 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -3,9 +3,6 @@ import streamlit as st
 import urllib.request
 import pandas as pd
 import numpy as np
-
-
-
 #url del archivo en formato raw
 url = 'https://raw.githubusercontent.com/CalistroAguilar24/PRUEBA/main/fallecidos_covid%20(3).csv'
 datos = pd.read_csv(url,sep= ',')
