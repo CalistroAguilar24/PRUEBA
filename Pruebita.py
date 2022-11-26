154321406f33c7ff0b64e443a1236014b1ac9a18
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-25 23:25:17 -0500

    Update Pruebita.py

--- Pruebita.py
+++ Pruebita.py
@@ -36,10 +36,9 @@ if selected == 'Inicio':
    st.dataframe(c)
    st.subheader("Características del Dataset")
    st.write(c.describe())
-   
-image = Image.open('imagen 1.jpg')
+   #url del archivo en formato raw
+   url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
+   datos = pd.read_csv(url,sep= ',')
+   st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
+   image = Image.open('imagen 1.jpg')
 
-#url del archivo en formato raw
-url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
-datos = pd.read_csv(url,sep= ',')
-st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
