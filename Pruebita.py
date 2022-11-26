1ee642cc1923e1691f6870a984323ee550e22233
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-25 19:30:16 -0500

    Update Pruebita.py

--- Pruebita.py
+++ Pruebita.py
@@ -21,12 +21,13 @@ if selected == 'Inicio':
    st.markdown("---")
    st.header("Dataset")
    
+   @st.experimental_memo
    def download_data():
-   url='https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv'
-   filename="Catalogo1960_2021.xlsx"
-   urllib.request.urlretrieve(url,filename)
-   df=pd.read_csv('Catalogo1960_2021.xlsx')
-   return df
+      url='https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv'
+      filename="Catalogo1960_2021.xlsx"
+      urllib.request.urlretrieve(url,filename)
+      df=pd.read_csv('Catalogo1960_2021.xlsx')
+      return df
    c=download_data()
    st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
    st.dataframe(c)
