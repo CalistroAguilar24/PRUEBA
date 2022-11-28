1a2aa6223cc759497dea962f82dfb64b3190d857
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-27 22:32:28 -0500

    Update Pruebita.py

--- Pruebita.py
+++ Pruebita.py
@@ -58,15 +58,13 @@ if selected == 'Informe':
 if selected == 'Equipo':
    st.markdown("<h1 style ='text-align: center'> Integrantes:</h1>", unsafe_allow_html= True)
    st.markdown("---")
-   col1, col2,col3 = st.columns(2)
+   col1, col2, col3= st.columns(3)
    image1 = Image.open('b9fd20744ad6f008787ffed46a0b7149--s-cartoons-bart-simpson.jpg')
    col1.header("Miguel Calistro")
    col1.image(image1, use_column_width=True)
    image2 = Image.open('273-2736237_20-lisa-simpson-tumblr-listening-to-headphones-pictures.png')
    col2.header("Brigytt Contreras")
    col2.image(image2, use_column_width=True)
-   
-   col3 = st.columns(1)
    image3 = Image.open('mqdefault.jpg')
    col3.header("Daniel Chamorro")
    col3.image(image3, use_column_width=True)
