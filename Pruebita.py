7f2a8203881b2d27075e3ff899da0be95919b49b
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-12-02 23:15:53 -0500

    Update Pruebita.py

--- Pruebita.py
+++ Pruebita.py
@@ -20,12 +20,14 @@ with st.sidebar:
       default_index = 0,
    )
 if selected == 'Inicio':
-   st.markdown("<h1 style ='text-align: center'> Introducción:</h1>", unsafe_allow_html= True)
+   st.markdown("<h1 style ='text-align: center'> CATALOGO SISMICO 1960-2021 (IGP) </h1>", unsafe_allow_html= True)
    st.markdown("---")
-   st.write("Los desastres naturales han acompañado el desarrollo de la humanidad a lo largo de la historia; por ello, rescatar y analizar a los sismos desde la historia ayuda a comprender no sólo las acciones humanas en torno a ellos, sino también a descifrar las características y patrones de comportamiento de la actividad sísmica, lo que puede ayudar a los sismólogos, geólogos y otros especialistas a elaborar con mayores datos y precisión, los mapas de riesgo.")
-   st.write(" Además, tener conocimiento sobre estos fenómenos nos permitirá construir una sociedad preventiva, la vulnerabilidad física y la vulnerabilidad social nos ayudará a contar con herramientas –como simulacros y mochilas de emergencia– para estar preparados ante lo que podemos esperar en un evento real.")          
-           
-
+   st.write('La base de datos sobre la actividad sísmica en el país fue realizada por el Instituto Geofísico del Perú (IGP) desde el año de 1960 hasta el 2021. El IGP es la institución responsable del monitoreo de la actividad sísmica del país, y contiene todos aquellos sismos percibidos por la población y registrados por la Red Sísmica Nacional desde 1960, fecha en la que se inicia la vigilancia instrumental de la sismicidad en el Perú.')
+   st.write('¿Por qué es importante saber sobre los datos de los sismos?')
+   col1, col2 =st.columns(2)
+   image = Image.open('imagen 1.jpg')
+   col1.write("Los desastres naturales han acompañado el desarrollo de la humanidad a lo largo de la historia; por ello, rescatar y analizar a los sismos desde la historia ayuda a comprender no sólo las acciones humanas en torno a ellos, sino también a descifrar las características y patrones de comportamiento de la actividad sísmica, lo que puede ayudar a los sismólogos, geólogos y otros especialistas a elaborar con mayores datos y precisión, los mapas de riesgo. Además, tener conocimiento sobre estos fenómenos nos permitirá construir una sociedad preventiva, la vulnerabilidad física y la vulnerabilidad social nos ayudará a contar con herramientas –como simulacros y mochilas de emergencia– para estar preparados ante lo que podemos esperar en un evento real.")          
+   col2.image(image,use_column_width=True)       
    def download_data():
       url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
       filename="Catalogo1960_2021.xlsx"
@@ -41,34 +43,60 @@ if selected == 'Inicio':
    url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
    datos = pd.read_csv(url,sep= ',')
    st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
-   image = Image.open('imagen 1.jpg')
-   st.image(image,use_column_width=True)
 if selected == 'Informe':
    st.markdown("<h1 style ='text-align: center'> CATÁLOGO SÍSMICO 1960-2021 (IGP):</h1>", unsafe_allow_html= True)
    st.markdown("---")
-   selected_year=st.sidebar.selectbox('Fecha', list(reversed(range(1960,2021))))
+   selected_year=st.sidebar.selectbox('Año', list(reversed(range(1960,2022))))
    def download_data():
       url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
       filename="Catalogo1960_2021.xlsx"
       urllib.request.urlretrieve(url,filename)
       df=pd.read_csv('Catalogo1960_2021.xlsx')
-      filt=(df["FECHA_UTC"] == selected_year)
-      df[filt]
-   download_data()
-if selected == 'Equipo':
-   st.markdown("<h1 style ='text-align: center'> ¿Quiénes somos?:</h1>", unsafe_allow_html= True)
-   st.markdown("---")
-   st.write('Somos un grupo de estudiantes del V ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruano Cayetano Heredia (UPCH). Mediante el procesamiento y visualización de datos ')
-   col1, col2, col3= st.columns(3)
-   image1 = Image.open('b9fd20744ad6f008787ffed46a0b7149--s-cartoons-bart-simpson.jpg')
-   col1.header("Miguel Calistro Aguilar")
-   col1.image(image1, use_column_width=True)
-   image2 = Image.open('273-2736237_20-lisa-simpson-tumblr-listening-to-headphones-pictures.png')
-   col2.header("Brigytt Contreras Melgar")
-   col2.image(image2, use_column_width=True)
-   image3 = Image.open('mqdefault.jpg')
-   col3.header("Daniel Chamorro Grados")
-   col3.image(image3, use_column_width=True)
+      return df
+   c=download_data()
+   def load_data(year):
+      df = download_data()
+      df = df.astype({'FECHA_UTC':'str'})
+      df['MAGNITUD']= pd.to_numeric(df['MAGNITUD'])
+      df['PROFUNDIDAD']= pd.to_numeric(df['PROFUNDIDAD'])
+      df['EPICENTRO']= pd.to_numeric(df['EPICENTRO'])
+      grouped = df.groupby(df.FECHA_UTC)
+      df_year = gruped.get_group(year)
+      return df_year
+   data_by_year= load_data(str(selected_year))
+   
+   sorted_unique_departament = sorted(data_by_year.DEPARTAMENTO.unique())
+   selected_departament=st.sidebar.multiselect('Departamento', sorted_unique_departament, sorted_unique_departament)
+      
+   unique_contaminant=['MAGNITUD', 'PROFUNDIDAD', 'EPICENTRO']
+   selected_carac=st.sidebar.multiselect('Características', unique_carac, unique_carac)
+   
+   df_selected=data_by_year[(data_by_year.DEPARTAMENTO.isin(selected_departament))]
+   
+   def remove_columns(dataset, cols):
+      return dataset.drop(cols, axis=1)
+   
+   cols=np.setdiff1d(unique_carac, selected_carac)
+   
+   st.subheader('Mostrar data de Magnitud , Profundidad y Epicentro')
+   data=remove_columns(df_selected, cols)
+   st.write('Dimensiones: ' + str(data.shape[0]) + ' filas y ' + str(data.shape[1]) + ' columnas')
+   st.dataframe(data)
+   
+#if selected == 'Equipo':
+   #st.markdown("<h1 style ='text-align: center'> ¿Quiénes somos?:</h1>", unsafe_allow_html= True)
+   #st.markdown("---")
+   #st.write('Somos un grupo de estudiantes del V ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruano Cayetano Heredia (UPCH). Mediante el procesamiento y visualización de datos se brindará todos los parámetros que caracterizan un sismo con el objetivo de constituirse como una base útil para la realización de estudios en sismología.')
+   #col1, col2, col3= st.columns(3)
+   #image1 = Image.open('f238361e-ccb8-48d1-8993-1de25db1c866.jpg')
+   #col1.header("Miguel Calistro Aguilar")
+   #col1.image(image1, use_column_width=True)
+   #image2 = Image.open('WhatsApp Image 2022-11-27 at 23.17.23.jpeg')
+   #col2.header("Brigytt Contreras Melgar")
+   #col2.image(image2, use_column_width=True)
+   #image3 = Image.open('WhatsApp Image 2022-11-28 at 19.18.22.jpeg')
+   #col3.header("Daniel Chamorro Grados")
+   #col3.image(image3, use_column_width=True)
    
    
 
