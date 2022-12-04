8e53f5362c678039445ef86423b8d48be86ba722
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-12-03 22:44:58 -0500

    Update Pruebita.py

--- Pruebita.py
+++ Pruebita.py
@@ -44,17 +44,17 @@ if selected == 'Informe':
    st.markdown("<h1 style ='text-align: center'> CATÁLOGO SÍSMICO 1960-2021 (IGP):</h1>", unsafe_allow_html= True)
    st.markdown("---")
    
-   def download_data():
-      url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
-      filename="Catalogo1960_2021.xlsx"
-      urllib.request.urlretrieve(url,filename)
-      df=pd.read_csv('Catalogo1960_2021.xlsx')
-      return df
-   c=download_data()
-   st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
-   st.dataframe(c)
-   st.subheader("Características del Dataset")
-   st.write(c.describe())
+   #def download_data():
+      #url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
+      #filename="Catalogo1960_2021.xlsx"
+      #urllib.request.urlretrieve(url,filename)
+      #df=pd.read_csv('Catalogo1960_2021.xlsx')
+      #return df
+   #c=download_data()
+   #st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
+   #st.dataframe(c)
+   #st.subheader("Características del Dataset")
+   #st.write(c.describe())
 
    #DATOS POR FECHA
    
@@ -67,8 +67,6 @@ if selected == 'Informe':
    datos_Amazonas= pd.read_csv('Amazona.csv')
    if opcion_dataset == 'AMAZONAS':
       df_visualizacion = datos_Amazonas
-   t1 = '• Cantidad de cuencas según los '+estado+'' 
-   st.dataframe(df_visualizacion)
    datos_Ancash= pd.read_csv('Ancash.csv')
    if opcion_dataset == 'ANCASH':
       df_visualizacion = datos_Ancash
